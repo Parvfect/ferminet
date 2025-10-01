fe41a011abc4aa67b2bca40d358f89eaddfee44a
Parvfect <parvagrw02@gmail.com>
2025-10-01 09:54:28 +0530

    Adding test systems for positron glue

---
+++ ferminet/configs/diatomic_seperated.py
@@ -0,0 +1,31 @@
+
+"""Positronic complex with a dilithium molecule."""
+
+from ferminet import base_config
+from ferminet.utils import system
+
+def get_config():
+    # Get default options.
+    cfg = base_config.default()
+    # Set up molecule
+    cfg.system.electrons = (10, 10, 0)
+    cfg.system.molecule = [
+        system.Atom("Ne", [0., 0., 0.], units='bohr'), 
+        system.Atom("Ne", [30.051, 0., 0.], units='bohr')
+    ]
+    # Set up positronic Hamiltonian. Indices of `fermion_charges` correspond to
+    # indices of `cfg.system.electrons`
+    cfg.system.make_local_energy_fn = \
+      'ferminet.positrons.hamiltonian.local_energy'
+    cfg.system.make_local_energy_kwargs = {
+        "fermion_charges": [-1, -1, 1],
+    }
+    # Use different move widths per particle species
+    cfg.mcmc.separate_spin_moves = True 
+    cfg.mcmc.move_width = 0.2
+    cfg.mcmc.burn_in = 100
+    cfg.mcmc.steps = 20
+    # Pretraining is not currently implemented for positronic systems
+    cfg.pretrain.method = 'none'
+
+    return cfg
