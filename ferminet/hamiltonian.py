6334545f8d3463b62752a0103a9ee5a45ac1009d
Parvfect <parvagrw02@gmail.com>
2025-10-26 11:38:18 +0530

    Turning my E back on

--- ferminet/hamiltonian.py
+++ ferminet/hamiltonian.py
@@ -283,7 +283,7 @@ def potential_electric_field(pos: Array, t: int, w: float):
   except:
     print("Don't think my time stepping is right")
     w2 = 1.0
-  return 0
+  #return 0
   return - sum([
     jnp.dot(E_vec, pos[k: k+3]) * E_max * jnp.sin(w * t) * w2 for k in range(
       0, pos.shape[0], 3)])
