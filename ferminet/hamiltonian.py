2cac5b7681ec13dc1145b5b5032a78369c2cdb07
Parvfect <parvagrw02@gmail.com>
2025-10-26 12:37:11 +0530

    Turning off E

--- ferminet/hamiltonian.py
+++ ferminet/hamiltonian.py
@@ -288,7 +288,8 @@ def potential_electric_field(pos: Array, t: int, w: float):
     w2 = 1.0
   """
   w2 = 1.0
-  #return 0
+  return 0
+  
   return - sum([
     jnp.dot(E_vec, pos[k: k+3]) * E_max * jnp.sin(w * t) * w2 for k in range(
       0, pos.shape[0], 3)])
