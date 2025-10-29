f5c88fc0ea6de2d379099b85d2edc3c69c95a1cc
Parvfect <parvagrw02@gmail.com>
2025-10-29 12:28:42 +0530

    Lowering E energy

--- ferminet/hamiltonian.py
+++ ferminet/hamiltonian.py
@@ -265,7 +265,7 @@ def excited_kinetic_energy_matrix(
 
 def potential_electric_field(pos: Array, t: int, w: float):
   E_vec = jnp.array([0.0, 0.0, 1.0]) # Unit vector along z direction
-  E_max = 5.0
+  E_max = 0.05
 
   t = t * 0.01 # Doing dt
 
