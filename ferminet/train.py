eb653362abbe82cf1843d2ac515ecc600daf7468
Parvfect <parvagrw02@gmail.com>
2025-11-24 21:43:11 +0530

    Making residual mean

--- ferminet/train.py
+++ ferminet/train.py
@@ -1219,7 +1219,7 @@ def train(
         
         #logging.info(f"{jnp.mean(theta_dot), jnp.max(theta_dot), jnp.min#(theta_dot), jnp.mean((theta_dot - jnp.mean(theta_dot) / n_params)**2)}")
         if t - t_init < 10:
-          logging.info(f"{r2}")
+          logging.info(f"{jnp.mean(r2)}")
     else:
       data, params, opt_state, loss, aux_data, pmove = step(
             data,
